66ffec5e8b66d3c35721e439baba0d69c5a9cbb9
sergio-rbz <s.marcello@rbz-schuetzenpark.de>
2026-09-02 14:44:53 +0200

    added some new comment

--- RBZ-Portal/src/test/java/newClass.java
+++ RBZ-Portal/src/test/java/newClass.java
@@ -3,4 +3,12 @@ public class newClass {
     //first comment
     //second comment
 
+
+    public static void main(String[] args) {
+    int prova = 123;
+        prova++;
+        System.out.println(prova);
+    }
+    //third comment
+
 }
